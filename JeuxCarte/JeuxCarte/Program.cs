e8f6312e2818f6052301ae53af7bb99b9e3a4d0b
geocot <martin.couture@uqtr.ca>
2017-09-23 12:24:05 -0400

    Debut

---
+++ JeuxCarte/JeuxCarte/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JeuxCarte
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
