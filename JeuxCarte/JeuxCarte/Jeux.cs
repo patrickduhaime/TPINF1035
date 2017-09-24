aab7fcf828b48d7c07b44824ff514e02fc04f18d
geocot <martin.couture@uqtr.ca>
2017-09-24 11:41:27 -0400

    Mise à jour du code

--- JeuxCarte/JeuxCarte/Jeux.cs
+++ JeuxCarte/JeuxCarte/Jeux.cs
@@ -6,7 +6,25 @@ namespace JeuxCarte
 {
     class Jeux
     {
+        //Déclartation des variables privées.
         private List<Carte> JeuxdeCartes = new List<Carte>();
+        private List<Carte> Pile = new List<Carte>();
+        private List<Carte> Pioche = new List<Carte>();
+        private bool PiocheVide;
+        private int NbJoueurs = 0;
+        private List<Joueur> OrdredesJoueurs = new List<Joueur>();
 
+        //Constructeur
+        public Jeux(int NbJoueur)
+        {
+            this.NbJoueurs = NbJoueur;
+        }
+
+
+        //Getter et Setter
+        public int NombreJoueurs
+        {
+            get { return this.NbJoueurs; }
+        }
     }
 }
