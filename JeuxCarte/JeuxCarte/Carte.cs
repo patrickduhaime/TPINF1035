ae55c8a72d1d186175fbf86d14c63510730ae743
geocot <martin.couture@uqtr.ca>
2017-09-24 10:20:23 -0400

    Fin de la création de la classe carte.

--- JeuxCarte/JeuxCarte/Carte.cs
+++ JeuxCarte/JeuxCarte/Carte.cs
@@ -9,10 +9,16 @@ namespace JeuxCarte
         //Définition des énumérations
         public enum enumSorte { Coeur, Pique, Treffle, Carreau }
         public enum enumValeur { As, deux, trois, quatre, cinq, six, sept, huit, neuf, dix, Valet, Dame, Roi}
-
+        
         //Déclaration des varibles encapsulées.
         private string sorte = "";
         private string valeur = "";
+
+        //Constructeur
+        public Carte( enumSorte sorte, enumValeur valeur ){
+            sorte = sorte;
+            valeur = valeur;
+        }
 
         //Getter et setter de la carte
         public string Sorte
@@ -25,6 +31,12 @@ namespace JeuxCarte
         {
             get { return valeur; }
             set { valeur = value; }
+        }
+
+        //Méthodes
+        public override string ToString()
+        {
+            return valeur + " de " + sorte;
         }
     }
 }
