a906378db2ab8e19f45b6ab647ce5aeb44376cda
geocot <martin.couture@uqtr.ca>
2017-10-01 14:43:06 -0400

    Clean le code

--- JeuxCarte/JeuxCarte/Program.cs
+++ JeuxCarte/JeuxCarte/Program.cs
@@ -153,58 +153,10 @@ namespace JeuxCarte
                                 System.Threading.Thread.Sleep(2000);
                                 LeJeux.PremierJoueur();
                                 break;
-
-                                //default:
-                                //Console.WriteLine("Entrer le nombre de joueur a creer entre 2 et 4");
-                                //break;
                         }
                         break;
-                        //default:
-                        //Console.WriteLine("Vous devez choisir entre 0 et 1");
-                        //break;
                 }
             }
-            //LeJeux.AjoutJoueur(new Joueur("Patrick", "Duhaime"));
-            //LeJeux.AjoutJoueur(new Joueur("Francis", "Picard"));
-            //LeJeux.AjoutJoueur(new Joueur("Martin", "Couture"));
-
-            //int i = 1;
-            //foreach (Joueur LeJoueur in LeJeux.GetListdesJoueurs)
-            //{
-            //    Console.WriteLine("Joueur" + (i++) + ": {0} {1}", LeJoueur.PrenomJoueur, LeJoueur.NomJoueur);
-            //}
-
-            /*Console.WriteLine("");
-
-            System.Threading.Thread.Sleep(2000);
-            Console.WriteLine("Les cartes sont distribues aux joueurs\n");
-            LeJeux.DistributCartes();
-            System.Threading.Thread.Sleep(2000);
-            LeJeux.PremierJoueur();
-
-            
-            Console.ReadKey(); //Permet d'arreter la console.
-
-
-            //Affiche l'état du jeux
-            Console.WriteLine("");
-            Console.WriteLine("**************************************************");
-            Console.WriteLine("*****************Etats du jeux********************");
-            Console.WriteLine("**************************************************");
-            Console.WriteLine("Le nombre de joueurs est {0}", LeJeux.GetNombreJoueurs);
-            foreach (Joueur LeJoueur in LeJeux.GetListdesJoueurs)
-            {
-                Console.WriteLine("Joueur: {0} {1}", LeJoueur.PrenomJoueur, LeJoueur.NomJoueur); 
-            }
-
-            foreach (Joueur LeJoueur in LeJeux.GetListdesJoueurs)
-            {
-                Console.WriteLine("Joueur: {0} {1}", LeJoueur.PrenomJoueur, LeJoueur.NomJoueur);
-            }
-            Console.WriteLine("Le nombre de cartes dans la pioche: {0} cartes", LeJeux.GetNombreCartePioche);
-            Console.WriteLine("Le jeux est brasse");
-            Console.WriteLine("Est-ce que la pioche est vide? {0}", LeJeux.GetEtatPiocheVide);
-            Console.ReadKey(); //Permet d'arreter la console.*/
         }
     }
 }
